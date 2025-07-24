62e9c5c1216f40bf143c2687b599723b67dd6423
Bishochiparaa <l352505686@gmail.com>
2025-07-24 16:42:53 +0800

    Update

--- src/flowOSD/Services/Hardware/PerformanceService.cs
+++ src/flowOSD/Services/Hardware/PerformanceService.cs
@@ -1,25 +1,4 @@
-﻿/*  Copyright © 2021-2024, Albert Akhmetov <akhmetov@live.com>   
- *
- *  This file is part of flowOSD.
- *
- *  flowOSD is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  flowOSD is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with flowOSD. If not, see <https://www.gnu.org/licenses/>.   
- *
- */
-
-namespace flowOSD.Services.Hardware;
-
-using System;
+﻿using System;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
@@ -30,244 +9,238 @@ using flowOSD.Core.Hardware;
 using flowOSD.Core.Resources;
 using flowOSD.Extensions;
 
-sealed class PerformanceService : IDisposable, IPerformanceService
+namespace flowOSD.Services.Hardware
 {
-    private CompositeDisposable? disposable = new CompositeDisposable();
-
-    private ITextResources textResources;
-    private IConfig config;
-    private INotificationService notificationService;
-    private IAtk atk;
-    private IPowerManagement powerManagement;
-
-    private BehaviorSubject<PerformanceProfile> activeProfileSubject;
-
-    // 新增：Turbo模式定时器
-    private Timer? turboRepeatTimer;
-
-    public PerformanceService(
-        ITextResources textResources,
-        IConfig config,
-        INotificationService notificationService,
-        IAtk atk,
-        IPowerManagement powerManagement)
+    sealed class PerformanceService : IDisposable, IPerformanceService
     {
-        this.textResources = textResources ?? throw new ArgumentNullException(nameof(textResources));
-        this.config = config ?? throw new ArgumentNullException(nameof(config));
-        this.atk = atk ?? throw new ArgumentNullException(nameof(atk));
-        this.notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
-        this.powerManagement = powerManagement ?? throw new ArgumentNullException(nameof(powerManagement));
+        private CompositeDisposable? disposable = new CompositeDisposable();
 
-        DefaultProfile = new PerformanceProfile(
-            PerformanceProfile.DefaultId,
-            textResources["PerformanceMode.Performance"],
-            PerformanceMode.Performance);
+        private readonly ITextResources textResources;
+        private readonly IConfig config;
+        private readonly INotificationService notificationService;
+        private readonly IAtk atk;
+        private readonly IPowerManagement powerManagement;
 
-        TurboProfile = new PerformanceProfile(
-            PerformanceProfile.TurboId,
-            textResources["PerformanceMode.Turbo"],
-            PerformanceMode.Turbo);
+        private readonly BehaviorSubject<PerformanceProfile> activeProfileSubject;
 
-        SilentProfile = new PerformanceProfile(
-            PerformanceProfile.SilentId,
-            textResources["PerformanceMode.Silent"],
-            PerformanceMode.Silent);
+        // 用来持有 Turbo 循环的订阅
+        private IDisposable? turboRepeater;
 
-        activeProfileSubject = new BehaviorSubject<PerformanceProfile>(DefaultProfile);
-        activeProfileSubject
-            .Skip(1)
-            .ObserveOn(SynchronizationContext.Current!)
-            .Subscribe(ApplyProfile)
-            .DisposeWith(disposable);
-
-        ActiveProfile = activeProfileSubject.AsObservable();
-
-        this.powerManagement.PowerSource.Throttle(TimeSpan.FromSeconds(2))
-            .CombineLatest(this.atk.TabletMode.Throttle(TimeSpan.FromSeconds(2)), (powerSource, tabletMode) => new { powerSource, tabletMode })
-            .Throttle(TimeSpan.FromMicroseconds(2000))
-            .ObserveOn(SynchronizationContext.Current!)
-            .Subscribe(x => ChangeActiveProfile(x.powerSource, x.tabletMode))
-            .DisposeWith(disposable);
-
-        this.config.Performance.PropertyChanged
-            .Where(IsActiveProfileProperty)
-            .Throttle(TimeSpan.FromMicroseconds(1000))
-            .ObserveOn(SynchronizationContext.Current!)
-            .Subscribe(_ => ChangeActiveProfile())
-            .DisposeWith(disposable);
-
-        this.config.Performance.ProfileChanged
-            .Throttle(TimeSpan.FromMicroseconds(1000))
-            .ObserveOn(SynchronizationContext.Current!)
-            .Subscribe(UpdateActiveProfile)
-            .DisposeWith(disposable);
-
-        this.atk.GpuMode
-            .Skip(1)
-            .Throttle(TimeSpan.FromMicroseconds(1000))
-            .ObserveOn(SynchronizationContext.Current!)
-            .Subscribe(_ => Update())
-            .DisposeWith(disposable);
-    }
-
-    public PerformanceProfile DefaultProfile { get; }
-
-    public PerformanceProfile TurboProfile { get; }
-
-    public PerformanceProfile SilentProfile { get; }
-
-    public IObservable<PerformanceProfile> ActiveProfile { get; }
-
-    public void Dispose()
-    {
-        StopTurboRepeatTimer();
-        disposable?.Dispose();
-        disposable = null;
-    }
-
-    public void Update()
-    {
-        ApplyProfile(activeProfileSubject.Value);
-    }
-
-    // 修改：只在Turbo模式时启动定时器
-    public void SetActiveProfile(Guid id)
-    {
-        var profile = GetProfile(id);
-        activeProfileSubject.OnNext(profile);
-
-        if (profile.PerformanceMode == PerformanceMode.Turbo)
+        public PerformanceService(
+            ITextResources textResources,
+            IConfig config,
+            INotificationService notificationService,
+            IAtk atk,
+            IPowerManagement powerManagement)
         {
-            StartTurboRepeatTimer(profile.Id);
-        }
-        else
-        {
-            StopTurboRepeatTimer();
-        }
-    }
+            this.textResources = textResources ?? throw new ArgumentNullException(nameof(textResources));
+            this.config = config ?? throw new ArgumentNullException(nameof(config));
+            this.atk = atk ?? throw new ArgumentNullException(nameof(atk));
+            this.notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
+            this.powerManagement = powerManagement ?? throw new ArgumentNullException(nameof(powerManagement));
 
-    // 新增：Turbo模式定时器启动
-    private void StartTurboRepeatTimer(Guid turboId)
-    {
-        StopTurboRepeatTimer(); // 防止重复启动
-        turboRepeatTimer = new Timer(_ =>
-        {
-            SetActiveProfile(turboId);
-        }, null, 20000, 20000); // 每20秒
-    }
+            DefaultProfile = new PerformanceProfile(
+                PerformanceProfile.DefaultId,
+                textResources["PerformanceMode.Performance"],
+                PerformanceMode.Performance);
 
-    // 新增：Turbo模式定时器停止
-    private void StopTurboRepeatTimer()
-    {
-        turboRepeatTimer?.Dispose();
-        turboRepeatTimer = null;
-    }
+            TurboProfile = new PerformanceProfile(
+                PerformanceProfile.TurboId,
+                textResources["PerformanceMode.Turbo"],
+                PerformanceMode.Turbo);
 
-    public PerformanceProfile GetProfile(Guid id)
-    {
-        if (id == DefaultProfile.Id)
-        {
-            return DefaultProfile;
-        }
-        else if (id == TurboProfile.Id)
-        {
-            return TurboProfile;
-        }
-        else if (id == SilentProfile.Id)
-        {
-            return SilentProfile;
-        }
-        else
-        {
-            return config.Performance[id] ?? DefaultProfile;
-        }
-    }
+            SilentProfile = new PerformanceProfile(
+                PerformanceProfile.SilentId,
+                textResources["PerformanceMode.Silent"],
+                PerformanceMode.Silent);
 
-    private async void ChangeActiveProfile()
-    {
-        var powerSource = await powerManagement.PowerSource.FirstAsync();
-        var tabletMode = await atk.TabletMode.FirstAsync();
+            activeProfileSubject = new BehaviorSubject<PerformanceProfile>(DefaultProfile);
+            activeProfileSubject
+                .Skip(1)
+                .ObserveOn(SynchronizationContext.Current!)
+                .Subscribe(ApplyProfile)
+                .DisposeWith(disposable);
 
-        ChangeActiveProfile(powerSource, tabletMode);
-    }
+            ActiveProfile = activeProfileSubject.AsObservable();
 
-    private void ChangeActiveProfile(PowerSource powerSource, TabletMode tabletMode)
-    {
-        Guid id;
+            this.powerManagement.PowerSource.Throttle(TimeSpan.FromSeconds(2))
+                .CombineLatest(this.atk.TabletMode.Throttle(TimeSpan.FromSeconds(2)), (powerSource, tabletMode) => new { powerSource, tabletMode })
+                .Throttle(TimeSpan.FromMilliseconds(2))
+                .ObserveOn(SynchronizationContext.Current!)
+                .Subscribe(x => ChangeActiveProfile(x.powerSource, x.tabletMode))
+                .DisposeWith(disposable);
 
-        if (tabletMode == TabletMode.Tablet)
-        {
-            id = config.Performance.TabletProfile;
-        }
-        else if (powerSource == PowerSource.Battery)
-        {
-            id = config.Performance.ChargerProfile;
-        }
-        else
-        {
-            id = config.Performance.BatteryProfile;
+            this.config.Performance.PropertyChanged
+                .Where(IsActiveProfileProperty)
+                .Throttle(TimeSpan.FromMilliseconds(1))
+                .ObserveOn(SynchronizationContext.Current!)
+                .Subscribe(_ => ChangeActiveProfile())
+                .DisposeWith(disposable);
+
+            this.config.Performance.ProfileChanged
+                .Throttle(TimeSpan.FromMilliseconds(1))
+                .ObserveOn(SynchronizationContext.Current!)
+                .Subscribe(UpdateActiveProfile)
+                .DisposeWith(disposable);
+
+            this.atk.GpuMode
+                .Skip(1)
+                .Throttle(TimeSpan.FromMilliseconds(1))
+                .ObserveOn(SynchronizationContext.Current!)
+                .Subscribe(_ => Update())
+                .DisposeWith(disposable);
         }
 
-        if (activeProfileSubject.Value.Id != id)
+        public PerformanceProfile DefaultProfile { get; }
+
+        public PerformanceProfile TurboProfile { get; }
+
+        public PerformanceProfile SilentProfile { get; }
+
+        public IObservable<PerformanceProfile> ActiveProfile { get; }
+
+        public void Dispose()
         {
-            SetActiveProfile(id);
+            turboRepeater?.Dispose();
+            disposable?.Dispose();
+            disposable = null;
         }
-    }
 
-    private void UpdateActiveProfile(Guid changedProfileId)
-    {
-        if (activeProfileSubject.Value.Id != changedProfileId)
+        public void Update()
         {
-            return;
+            ApplyProfile(activeProfileSubject.Value);
         }
 
-        SetActiveProfile(changedProfileId);
-    }
-
-    private bool IsActiveProfileProperty(string? propertyName)
-    {
-        return
-            propertyName == nameof(PerformanceConfig.ChargerProfile) ||
-            propertyName == nameof(PerformanceConfig.BatteryProfile) ||
-            propertyName == nameof(PerformanceConfig.TabletProfile);
-    }
-
-    private async void ApplyProfile(PerformanceProfile profile)
-    {
-        atk.SetPerformanceMode(profile.PerformanceMode);
-
-        if (profile.IsUserProfile && !await SetCustomProfile(profile))
+        public void SetActiveProfile(Guid id)
         {
-            Common.TraceWarning("Can't set custom profile");
-
-            activeProfileSubject.OnNext(DefaultProfile);
-        }
-    }
-
-    private async Task<bool> SetCustomProfile(PerformanceProfile profile)
-    {
-        var gpuMode = await atk.GpuMode.FirstAsync();
-        if (profile.UseCustomFanCurves && (gpuMode == GpuMode.dGpu || config.Common.ForceCustomFanCurves))
-        {
-            if (!atk.SetFanCurve(FanType.Cpu, profile.CpuFanCurve))
+            // 如果是 Turbo，启动或保持每20秒重复设置 Turbo
+            if (id == TurboProfile.Id)
             {
-                Common.TraceWarning("Can't set CPU Fan Curve");
-                return false;
+                if (turboRepeater == null)
+                {
+                    turboRepeater = Observable
+                        .Interval(TimeSpan.FromSeconds(20))
+                        .ObserveOn(SynchronizationContext.Current!)
+                        .Subscribe(_ => activeProfileSubject.OnNext(TurboProfile));
+
+                    disposable?.Add(turboRepeater);
+                }
+            }
+            else
+            {
+                // 切换到其他档位时，停止 Turbo 循环
+                turboRepeater?.Dispose();
+                turboRepeater = null;
             }
 
-            if (!atk.SetFanCurve(FanType.Gpu, profile.GpuFanCurve))
+            // 推送本次切换
+            activeProfileSubject.OnNext(GetProfile(id));
+        }
+
+        public PerformanceProfile GetProfile(Guid id)
+        {
+            if (id == DefaultProfile.Id)
             {
-                Common.TraceWarning("Can't set GPU Fan Curve");
-                return false;
+                return DefaultProfile;
+            }
+            else if (id == TurboProfile.Id)
+            {
+                return TurboProfile;
+            }
+            else if (id == SilentProfile.Id)
+            {
+                return SilentProfile;
+            }
+            else
+            {
+                return config.Performance[id] ?? DefaultProfile;
             }
         }
 
-        if (!atk.SetCpuLimit(profile.CpuLimit))
+        private async void ChangeActiveProfile()
         {
-            Common.TraceWarning("Can't set CPU Power Limit");
-            return false;
+            var powerSource = await powerManagement.PowerSource.FirstAsync();
+            var tabletMode = await atk.TabletMode.FirstAsync();
+
+            ChangeActiveProfile(powerSource, tabletMode);
         }
 
-        return true;
+        private void ChangeActiveProfile(PowerSource powerSource, TabletMode tabletMode)
+        {
+            Guid id;
+
+            if (tabletMode == TabletMode.Tablet)
+            {
+                id = config.Performance.TabletProfile;
+            }
+            else if (powerSource == PowerSource.Battery)
+            {
+                id = config.Performance.ChargerProfile;
+            }
+            else
+            {
+                id = config.Performance.BatteryProfile;
+            }
+
+            if (activeProfileSubject.Value.Id != id)
+            {
+                SetActiveProfile(id);
+            }
+        }
+
+        private void UpdateActiveProfile(Guid changedProfileId)
+        {
+            if (activeProfileSubject.Value.Id != changedProfileId)
+            {
+                return;
+            }
+
+            SetActiveProfile(changedProfileId);
+        }
+
+        private bool IsActiveProfileProperty(string? propertyName)
+        {
+            return propertyName == nameof(PerformanceConfig.ChargerProfile)
+                || propertyName == nameof(PerformanceConfig.BatteryProfile)
+                || propertyName == nameof(PerformanceConfig.TabletProfile);
+        }
+
+        private async void ApplyProfile(PerformanceProfile profile)
+        {
+            atk.SetPerformanceMode(profile.PerformanceMode);
+
+            if (profile.IsUserProfile && !await SetCustomProfile(profile))
+            {
+                Common.TraceWarning("Can't set custom profile");
+                activeProfileSubject.OnNext(DefaultProfile);
+            }
+        }
+
+        private async Task<bool> SetCustomProfile(PerformanceProfile profile)
+        {
+            var gpuMode = await atk.GpuMode.FirstAsync();
+            if (profile.UseCustomFanCurves && (gpuMode == GpuMode.dGpu || config.Common.ForceCustomFanCurves))
+            {
+                if (!atk.SetFanCurve(FanType.Cpu, profile.CpuFanCurve))
+                {
+                    Common.TraceWarning("Can't set CPU Fan Curve");
+                    return false;
+                }
+
+                if (!atk.SetFanCurve(FanType.Gpu, profile.GpuFanCurve))
+                {
+                    Common.TraceWarning("Can't set GPU Fan Curve");
+                    return false;
+                }
+            }
+
+            if (!atk.SetCpuLimit(profile.CpuLimit))
+            {
+                Common.TraceWarning("Can't set CPU Power Limit");
+                return false;
+            }
+
+            return true;
+        }
     }
 }
--- src/flowOSD/UI/Commands/PerformanceCommand.cs
+++ src/flowOSD/UI/Commands/PerformanceCommand.cs
@@ -1,87 +1,112 @@
-﻿using System;
+﻿/*  Copyright © 2021-2024, Albert Akhmetov <akhmetov@live.com>   
+ *
+ *  This file is part of flowOSD.
+ *
+ *  flowOSD is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  flowOSD is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with flowOSD. If not, see <https://www.gnu.org/licenses/>.   
+ *
+ */
+
+namespace flowOSD.UI.Commands;
+
 using System.ComponentModel;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
-using System.Windows.Threading;       // 注意这里
 using flowOSD.Core.Configs;
 using flowOSD.Core.Hardware;
 using flowOSD.Core.Resources;
 using flowOSD.Extensions;
 
-namespace flowOSD.UI.Commands
+public class PerformanceCommand : CommandBase
 {
-    public class PerformanceCommand : CommandBase, IDisposable
+    private IConfig config;
+    private IAtk atk;
+    private IPowerManagement powerManagement;
+    private IPerformanceService performanceService;
+
+    public PerformanceCommand(
+        ITextResources textResources,
+        IImageResources imageResources,
+        IConfig config,
+        IAtk atk,
+        IPowerManagement powerManagement,
+        IPerformanceService performanceService) 
+        : base(
+            textResources,
+            imageResources)
     {
-        private readonly IConfig config;
-        private readonly IAtk atk;
-        private readonly IPowerManagement powerManagement;
-        private readonly IPerformanceService performanceService;
-        private readonly CompositeDisposable disposables = new CompositeDisposable();
-        private readonly DispatcherTimer dispatcherTimer;
+        this.config = config ?? throw new ArgumentNullException(nameof(config));
+        this.atk = atk ?? throw new ArgumentNullException(nameof(atk));
+        this.powerManagement = powerManagement ?? throw new ArgumentNullException(nameof(powerManagement));
+        this.performanceService = performanceService ?? throw new ArgumentNullException(nameof(performanceService));
 
-        public PerformanceCommand(
-            ITextResources textResources,
-            IImageResources imageResources,
-            IConfig config,
-            IAtk atk,
-            IPowerManagement powerManagement,
-            IPerformanceService performanceService)
-            : base(textResources, imageResources)
+        this.performanceService.ActiveProfile
+            .ObserveOn(SynchronizationContext.Current!)
+            .Subscribe(profile => IsChecked = profile.Id != PerformanceProfile.DefaultId)
+            .DisposeWith(Disposable!);
+
+        Description = TextResources["Commands.Performance.Description"];
+        Enabled = true;
+    }
+
+    public override bool CanExecuteWithHotKey => true;
+
+    public override async void Execute(object? parameter = null)
+    {
+        if (!Enabled)
         {
-            this.config             = config ?? throw new ArgumentNullException(nameof(config));
-            this.atk                = atk ?? throw new ArgumentNullException(nameof(atk));
-            this.powerManagement    = powerManagement ?? throw new ArgumentNullException(nameof(powerManagement));
-            this.performanceService = performanceService ?? throw new ArgumentNullException(nameof(performanceService));
-
-            // 同步菜单勾选状态
-            performanceService.ActiveProfile
-                .ObserveOn(Dispatcher.CurrentDispatcher)
-                .Subscribe(profile => IsChecked = profile.Id != PerformanceProfile.DefaultId)
-                .DisposeWith(disposables);
-
-            Description = TextResources["Commands.Performance.Description"];
-            Enabled     = true;
-
-            // UI 线程上的定时器：立刻启动，20s 间隔
-            dispatcherTimer = new DispatcherTimer(
-                TimeSpan.FromSeconds(20),
-                DispatcherPriority.Normal,
-                (s, e) => Execute(PerformanceProfile.TurboId),
-                Dispatcher.CurrentDispatcher);
-
-            dispatcherTimer.Start();
+            return;
         }
 
-        public override bool CanExecuteWithHotKey => true;
-
-        /// <summary>
-        /// 专门处理 Guid 参数：每次都会收到 TurboId，直接切档并保存
-        /// </summary>
-        public override async void Execute(object? parameter = null)
+        if (parameter is Guid profileId)
         {
-            if (!Enabled || parameter is not Guid profileId)
-                return;
-
             performanceService.SetActiveProfile(profileId);
             await SaveActiveProfile(profileId);
         }
-
-        private async Task SaveActiveProfile(Guid profileId)
+        else
         {
-            // 根据当前模式（平板/插电/电池）写入不同配置项
-            if (await atk.TabletMode.FirstOrDefaultAsync() == TabletMode.Tablet)
-                config.Performance.TabletProfile = profileId;
-            else if (await powerManagement.PowerSource.FirstOrDefaultAsync() == PowerSource.Battery)
-                config.Performance.ChargerProfile = profileId;
-            else
-                config.Performance.BatteryProfile = profileId;
-        }
-
-        public void Dispose()
-        {
-            dispatcherTimer.Stop();
-            disposables.Dispose();
+            var nextProfileId = await GetNextProfileId();
+            performanceService.SetActiveProfile(nextProfileId);
+            await SaveActiveProfile(nextProfileId);
         }
     }
+
+    private async Task SaveActiveProfile(Guid profileId)
+    {
+        if (await atk.TabletMode.FirstOrDefaultAsync() == TabletMode.Tablet)
+        {
+            config.Performance.TabletProfile = profileId;
+        }
+        else if (await powerManagement.PowerSource.FirstOrDefaultAsync() == PowerSource.Battery)
+        {
+            config.Performance.ChargerProfile = profileId;
+        }
+        else
+        {
+            config.Performance.BatteryProfile = profileId;
+        }
+    }
+
+private async Task<Guid> GetNextProfileId()
+{
+    var profile = await performanceService.ActiveProfile.FirstAsync();
+    if (profile.Id == PerformanceProfile.DefaultId)
+    {
+        return PerformanceProfile.TurboId;
+    }
+    // 其余情况一律返回 TurboId
+    return PerformanceProfile.TurboId;
+}
+
 }
